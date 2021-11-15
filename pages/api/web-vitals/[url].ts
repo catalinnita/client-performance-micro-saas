c5bd66dd90b6d6898d90351f9d89612d8297a86f
DESKTOP-P579E7L\catalin <catalin@incsub.com>
2021-11-15 11:09:05 +0000

    add first endpoint

--- pages/api/web-vitals/[url].ts
+++ pages/api/web-vitals/[url].ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Url } from 'url';
 import { generateLighthouseReport } from '../../../utils/generateLighthouseReport'
 
 type Data = {
--- utils/generateLighthouseReport.ts
+++ utils/generateLighthouseReport.ts
@@ -10,21 +10,23 @@ export const generateLighthouseReport = async (url: string) => {
     const options = {
         logLevel: "info",
         disableDeviceEmulation: true,
-        chromeFlags: ['--disable-mobile-emulation']
+        chromeFlags: ['--disable-mobile-emulation'],
+        onlyCategories: ['performance']
     } as Options
-    console.log({chromeLauncher})
+
     const chrome = await chromeLauncher.launch(options);
-    options.port = chrome.port;
+    options.port = chrome.port
 
     // Connect chrome-launcher to puppeteer
-    const resp = await util.promisify(request)(`http://localhost:${options.port}/json/version`);
+    const resp = await util.promisify(request)(`http://localhost:${options.port}/json/version`)
     const { webSocketDebuggerUrl } = JSON.parse(resp.body);
-    const browser = await puppeteer.connect({ browserWSEndpoint: webSocketDebuggerUrl });
+    const browser = await puppeteer.connect({ browserWSEndpoint: webSocketDebuggerUrl })
 
     // Run Lighthouse
-    const { lhr } = await lighthouse(url, options, null);
-    browser.disconnect();
-    await chrome.kill();
+    const { lhr } = await lighthouse(url, options, null)
+    browser.disconnect()
+    await chrome.kill()
 
-    return reportGenerator.generateReport(lhr, 'json');
+    const { audits } = JSON.parse(reportGenerator.generateReport(lhr, 'json'))
+    return audits
 }
